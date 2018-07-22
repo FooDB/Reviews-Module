5a670ab0ed5ec9b875ba146aa02ff82046a32fb6
Chris W <christopherkwildenradt@gmail.com>
2018-07-22 14:19:32 -0700

    Add elipsis bubbles

--- client/components/Pagination.jsx
+++ client/components/Pagination.jsx
@@ -10,25 +10,29 @@ class Pagination extends React.Component {
     render() {
         let nextPageArrow = (this.props.currentPage === this.props.totalPages ? this.props.currentPage : this.props.currentPage + 1);
         let previousPageArrow = (this.props.currentPage === 1 ? this.props.currentPage : this.props.currentPage - 1);
+        let firstElipsis = (this.props.currentPage - 3 > 0 
+            ? <span><span className="paginationBubble">...</span></span>
+            : '')
+        let secondElipsis = (this.props.currentPage + 4 >= this.props.totalPages 
+            ? <span><span className="paginationBubble">...</span></span>
+            : '')
         return (
             <div id="paginationContainer">
                 <span>
                     <span className="paginationArrow" onClick={() => this.props.handlePageChange(previousPageArrow)}>{'<'}</span>
                 </span>
-
                 <span>
                     <span>
-                        <span className="paginationBubble" onClick={() => this.props.handlePageChange(this.props.currentPage - 1)}>{this.props.currentPage}</span>
+                        <span className="paginationBubble" onClick={() => this.props.handlePageChange(1)}>1</span>
                     </span>
+                    {firstElipsis}
                     <span>
                         <span className="paginationBubble" onClick={() => this.props.handlePageChange(this.props.currentPage + 1)}>{this.props.currentPage + 1}</span>
                     </span>
                     <span>
                         <span className="paginationBubble" onClick={() => this.props.handlePageChange(this.props.currentPage + 2)}>{this.props.currentPage + 2}</span>
                     </span>
-                    <span>
-                        <span className="paginationBubble">...</span>
-                    </span>
+                    {secondElipsis}
                     <span>
                         <span className="paginationBubble" onClick={() => this.props.handlePageChange(this.props.totalPages)}>{this.props.totalPages}</span>
                     </span>
