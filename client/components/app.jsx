cafe4cbfbc77c18b00e69db86735e0528cc3c0de
Chris W <christopherkwildenradt@gmail.com>
2018-07-22 13:10:38 -0700

    Remove unnecessary styles import

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReviewList from './ReviewList.jsx';
-import '../../public/styles.css';
 import ReviewSummary from './ReviewSummary.jsx';
 import ReviewToolbar from './ReviewToolbar.jsx';
 
