5e9c1c0a374c7e9275191785282a3313419a1cc6
Chris W <christopherkwildenradt@gmail.com>
2018-07-24 10:01:34 -0700

    Recreate new review html template

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -68,61 +68,132 @@ class Review extends React.Component {
         if (!this.state.readMoreClicked && this.props.review.reviewText.length < 300) readMorePhrase = '';
 
         return (
-            <div id="reviewContainer">
-                {this.state.reportPopUp}
-                <div>
-                    <div>
-                        <span>
-                            <div>
-                                <div className="authorArea">
-                                    <span><img className="imgCircle" src={this.props.review.userPhoto} /></span> 
+            <div id="total review">
+                <div id="review with padding">
+
+                    <div id="leftHalfAuthorArea">
+                        <div id="circleContainer">
+                            <div id="circle">
+                                <div id="initials">CK</div>
+                            </div>
+                        </div>
+                        <div>
+                            <span>
+                                <span>UserName</span>
+                            </span>
+                        </div>
+                        <span>UserCity</span>
+                        <div>
+                            <span>Comment Icon</span>
+                            <span># of reviews</span>
+                        </div>
+                    </div>
+
+                    <div id="rightHalf">
+
+                        <div id="starsDateRating">
+
+                            <div id="starsAndDate">
+                                <div id="stars">
+                                    <span><img className="star" src={this.state.stars[0]} /></span>
+                                    <span><img className="star" src={this.state.stars[1]} /></span>
+                                    <span><img className="star" src={this.state.stars[2]} /></span>
+                                    <span><img className="star" src={this.state.stars[3]} /></span>
+                                    <span><img className="star" src={this.state.stars[4]} /></span>
                                 </div>
-                                <div className="authorArea">
-                                    <div>
-                                        <span>{this.props.review.userName} &nbsp;</span>
-                                        <span> ({this.props.review.userArea})</span>
-                                    </div>
-                                    <div id="starsAndDate">
-                                        <span><img className="star" src={this.state.stars[0]} /></span>
-                                        <span><img className="star" src={this.state.stars[1]} /></span>
-                                        <span><img className="star" src={this.state.stars[2]} /></span>
-                                        <span><img className="star" src={this.state.stars[3]} /></span>
-                                        <span><img className="star" src={this.state.stars[4]} /></span>
-                                        <span className="ratingDate"> {this.props.review.overallRating}.0 </span>
-                                        <span className="ratingDate"> Dined on {new Date(reviewDate[0], reviewDate[1] - 1, reviewDate[2].substr(0,2)).toDateString()}</span>
-                                    </div>
+                                <span>Dined: Date</span>
+                            </div>
+                            <div id="ratings">
+                                <span>Overall</span>
+                                <span>5</span>
+                                <span>Food</span>
+                                <span>5</span>
+                                <span>Service</span>
+                                <span>5</span>
+                                <span>Ambiance</span>
+                                <span>5</span>
+                            </div>
+
+                        </div>
+                        <div>
+                            <p>Review Text</p>
+                        </div>
+
+                        <div id="readmoreLink and reportHelpful">
+                            <div>
+                                <a>Read More</a>
+                            </div>
+
+                            <div id="reportHelpfulContainer">
+                                <div>
+                                    <div>flagIcon</div>
+                                    <div>Report</div>
+                                </div>
+                                <div>
+                                    <div>helpful Icon</div>
+                                    <div>Helpful</div>
                                 </div>
                             </div>
-                        </span>
-                    </div>
-                </div>
-
-                <div>
-                    <p id="reviewText">{this.state.reviewText}</p>
-                </div>
-                <div  id="reportHelpful">
-                    <div>
-                        <a id="readMore" href="#" onClick={(e) => this.readMoreToggle(e)}>{readMorePhrase}</a>
-                    </div>
-                    <div id="subReportHelpful">
-                        <span className="flex" onClick={(e) => this.toggleReportModal(e)}>
-                            <div id="flagIcon"></div>
-                            <span id="reportText">Report</span>
-                        </span>
-                        <span className="flex" id={helpHover} 
-                              onClick={() => this.helpfulClick(this.props.review.is_helpful)} 
-                              onMouseOver={() => this.setState({hoveronHelp: true, upvoteIcon: './images/redUpvote.png'})} 
-                              onMouseLeave={() => {
-                                    this.setState({hoveronHelp: false});
-                                    this.state.helpful ? this.setState({upvoteIcon: './images/redUpvote.png'}) : this.setState({upvoteIcon: './images/whiteUpvote.png'})
-                                }}
-                              value={this.props.review.is_helpful}>
-                            <div className="flex" ><img id="upvoteIcon" src={this.state.upvoteIcon} /></div>
-                            <span className="flex">&nbsp; Helpful {this.state.helpful ? '(1)' : ''}</span>
-                        </span>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            // <div id="reviewContainer">
+            //     {this.state.reportPopUp}
+            //     <div>
+            //         <div>
+            //             <span>
+            //                 <div>
+            //                     <div className="authorArea">
+            //                         <span><img className="imgCircle" src={this.props.review.userPhoto} /></span> 
+            //                     </div>
+            //                     <div className="authorArea">
+            //                         <div>
+            //                             <span>{this.props.review.userName} &nbsp;</span>
+            //                             <span> ({this.props.review.userArea})</span>
+            //                         </div>
+            //                         <div id="starsAndDate">
+            //                             <span><img className="star" src={this.state.stars[0]} /></span>
+            //                             <span><img className="star" src={this.state.stars[1]} /></span>
+            //                             <span><img className="star" src={this.state.stars[2]} /></span>
+            //                             <span><img className="star" src={this.state.stars[3]} /></span>
+            //                             <span><img className="star" src={this.state.stars[4]} /></span>
+            //                             <span className="ratingDate"> {this.props.review.overallRating}.0 </span>
+            //                             <span className="ratingDate"> Dined on {new Date(reviewDate[0], reviewDate[1] - 1, reviewDate[2].substr(0,2)).toDateString()}</span>
+            //                         </div>
+            //                     </div>
+            //                 </div>
+            //             </span>
+            //         </div>
+            //     </div>
+
+            //     <div>
+            //         <p id="reviewText">{this.state.reviewText}</p>
+            //     </div>
+            //     <div  id="reportHelpful">
+            //         <div>
+            //             <a id="readMore" href="#" onClick={(e) => this.readMoreToggle(e)}>{readMorePhrase}</a>
+            //         </div>
+            //         <div id="subReportHelpful">
+            //             <span className="flex" onClick={(e) => this.toggleReportModal(e)}>
+            //                 <div id="flagIcon"></div>
+            //                 <span id="reportText">Report</span>
+            //             </span>
+            //             <span className="flex" id={helpHover} 
+            //                   onClick={() => this.helpfulClick(this.props.review.is_helpful)} 
+            //                   onMouseOver={() => this.setState({hoveronHelp: true, upvoteIcon: './images/redUpvote.png'})} 
+            //                   onMouseLeave={() => {
+            //                         this.setState({hoveronHelp: false});
+            //                         this.state.helpful ? this.setState({upvoteIcon: './images/redUpvote.png'}) : this.setState({upvoteIcon: './images/whiteUpvote.png'})
+            //                     }}
+            //                   value={this.props.review.is_helpful}>
+            //                 <div className="flex" ><img id="upvoteIcon" src={this.state.upvoteIcon} /></div>
+            //                 <span className="flex">&nbsp; Helpful {this.state.helpful ? '(1)' : ''}</span>
+            //             </span>
+            //         </div>
+            //     </div>
+            // </div>
         )
     }
 }
