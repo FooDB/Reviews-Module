58bad62d2560711a935ed342f2c99f217ecc3c08
Chris W <christopherkwildenradt@gmail.com>
2018-07-25 14:32:00 -0700

    Style new dropdown

--- client/components/ReviewToolbar.jsx
+++ client/components/ReviewToolbar.jsx
@@ -8,33 +8,36 @@ class ReviewToolbar extends React.Component {
             ddSelected: false,
             display: 'none',
             current: 'Newest',
+            arrow: './images/downArrow.png'
         }
     }
     handleSelection(e) {
-        console.log('handleselection called', e.target.textContent)
-        this.setState({ddSelected: !this.state.ddSelected, current: e.target.textContent})
-        !this.state.ddSelected ? this.setState({display: 'block'}) : this.setState({display: 'none'})
-        // this.props.sortReviews(e.target.value);
+        console.log('handleselection called', e.target.textContent);
+        this.setState({ddSelected: !this.state.ddSelected});
+        e.target.textContent ? this.setState({current: e.target.textContent}) : null;
+        if (!this.state.ddSelected) {
+            this.setState({display: 'block'})
+            this.setState({arrow: './images/upArrow.png'})
+        } else {
+            this.setState({display: 'none'});
+            this.setState({arrow: './images/downArrow.png'})
+        } 
     }
     render() {
         return (
             <div id="toolbarContainer">
                 <div id="toolbarSortText">Sort By</div>
-                <div id="dropdownHeader" onClick={(e) => this.handleSelection(e)}>{this.state.current}</div>
-                <div>
-                    <div className="dropdownItem" value="Newest" onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} style={{display: this.state.display}}>Newest</div>
-                    <div className="dropdownItem" value="Highest"onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} style={{display: this.state.display}}>Highest Rating</div>
-                    <div className="dropdownItem" value="Lowest" onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} style={{display: this.state.display}}>Lowest Rating</div>
+
+                <div id="dropdownHeader" onClick={(e) => this.handleSelection(e)}>
+                    <span>{this.state.current}</span>
+                    <span><img className="star" src={this.state.arrow} /></span>
                 </div>
-    
-    
-                <div id="sortSelection">
-                    <select id="sortMethod" onChange={() => this.props.sortReviews()}>
-                        <option value="Newest">Newest</option>
-                        <option value="Highest">Highest Rating</option>
-                        <option value="Lowest">Lowest Rating</option>
-                    </select>
+                <div className="dropdownContainer" style={{display: this.state.display}}>
+                    <div className="dropdownItem" value="Newest" onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} >Newest</div>
+                    <div className="dropdownItem" value="Highest"onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} >Highest Rating</div>
+                    <div className="dropdownItem" value="Lowest" onClick={(e) => {this.props.sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e)}} >Lowest Rating</div>
                 </div>
+
                 <div id="filtersHeader">Filters</div>
                 <div>
                     {this.props.keyWords.map(keyWord => <FilterBox keyWord={keyWord} key={keyWord.id} filterReviews={this.props.filterReviews}/>)}
