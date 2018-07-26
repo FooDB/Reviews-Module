c4d60fc007a10e03d2f1cbf287e908e3f005cf32
Chris W <christopherkwildenradt@gmail.com>
2018-07-25 19:19:11 -0700

    Add prop-types

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Review from './Review.jsx';
 
-const ReviewList = props => (
+const ReviewList = ({ reviews }) => (
   <div id="reviewsContainer">
-    {props.reviews.map(review => <Review review={review} key={review.id} />)}
+    {reviews.map(review => <Review review={review} key={review.id} />)}
   </div>
-)
+);
 
 export default ReviewList;
