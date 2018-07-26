1c832f5dcfd39093f682c05a7073d03040763c6a
Chris W <christopherkwildenradt@gmail.com>
2018-07-25 17:37:08 -0700

    Beautify App and Error components

--- client/components/FilterBox.jsx
+++ client/components/FilterBox.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 
 class FilterBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            icon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/emptyBox.png',
-            redIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redBox.png',
-            whiteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/emptyBox.png',
-            clicked: false,
-            style: 'none'
-        }
-    }
-    switchIcon() {
-        this.state.clicked = !this.state.clicked;
-        if(this.state.clicked) {
-            this.setState({icon: this.state.redIcon})
-        } else {
-            this.setState({icon: this.state.whiteIcon});
-            document.activeElement.blur();
-        } 
-    }
-    render() {
-        return (
-        <span className="filterCheckBox" tabIndex="0" onClick={() => {this.switchIcon(); this.props.filterReviews(this.props.keyWord.filterKeyword)}}>
-            <span><img className="filterBoxIcon" src={this.state.icon} /> </span>
-            <span> {this.props.keyWord.filterKeyword}</span>
-        </span>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      icon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/emptyBox.png',
+      redIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redBox.png',
+      whiteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/emptyBox.png',
+      clicked: false,
+    };
+  }
+
+  switchIcon() {
+    this.state.clicked = !this.state.clicked;
+    if(this.state.clicked) {
+      this.setState({icon: this.state.redIcon})
+    } else {
+      this.setState({icon: this.state.whiteIcon});
+      document.activeElement.blur();
+    } 
+  }
+
+  render() {
+    return (
+      <span className="filterCheckBox" tabIndex="0" onClick={() => {this.switchIcon(); this.props.filterReviews(this.props.keyWord.filterKeyword)}}>
+          <span><img className="filterBoxIcon" src={this.state.icon} /> </span>
+          <span> {this.props.keyWord.filterKeyword}</span>
+      </span>
+    )
+  }
 }
 export default FilterBox;
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,180 +6,189 @@ import Pagination from './Pagination.jsx';
 import ErrorBoundary from './Error.jsx';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            reviews: [],
-            allReviews: [],
-            filteredReviews: [],
-            keyWords: [],
-            lovedFor: [],
-            ratings: {
-                totalAverage: 0,
-                foodAverage: 0,
-                serviceAverage: 0,
-                ambianceAverage: 0,
-                valueAverage: 0,
-                noise: 0,
-                recommended: 0
-            },
-            is_filtered: false,
-            stars: [],
-            currentPage: 1,
-            totalPages: 0,
-            restaurantInfo: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+      allReviews: [],
+      filteredReviews: [],
+      keyWords: [],
+      lovedFor: [],
+      ratings: {
+        totalAverage: 0,
+        foodAverage: 0,
+        serviceAverage: 0,
+        ambianceAverage: 0,
+        valueAverage: 0,
+        noise: 0,
+        recommended: 0
+      },
+      is_filtered: false,
+      stars: [],
+      currentPage: 1,
+      totalPages: 0,
+      restaurantInfo: []
+    };
+  }
+
+  componentDidMount() {
+    this.pullKeywordsById();
+    this.pullMenuItemsById();
+    this.pullDataById();
+    this.pullRestaurantInfoById();
+  }
+
+  getAverage(reviews, criteria) {
+    let sum = 0;
+    for (let i = 0; i < reviews.length; i++) {
+      sum += reviews[i][criteria];
     }
-    componentDidMount() {
-        this.pullKeywordsById();
-        this.pullMenuItemsById();
-        this.pullDataById();
-        this.pullRestaurantInfoById();
+    return Number.parseFloat(sum / reviews.length).toFixed(1);
+  }
+
+  setDynamicStarRating() {
+    let totalAverageCopy = this.state.ratings.totalAverage;
+    let starsToGo = false;
+    for (let i = 0; i < 5; i++) {
+      if (totalAverageCopy - 1 < 0 && !starsToGo) {
+        totalAverageCopy > .5 ? this.state.stars.push('https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/highStar.png') : this.state.stars.push('https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/lowStar.png');
+        totalAverageCopy--;
+        starsToGo = true;
+      } else {
+        totalAverageCopy > 0 ? this.state.stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redStar.png") : this.state.stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/greyStar.png");
+        totalAverageCopy--;
+      }
     }
-    getAverage(reviews, criteria) {
-        let sum = 0;
-        for (let i = 0; i < reviews.length; i++) {
-            sum += reviews[i][criteria]
-        }
-        return Number.parseFloat(sum / reviews.length).toFixed(1);
-    }
-    pullDataById() {
-        console.log('pulled data called');
-        axios.get(`/reviews/${3}`)
-        .then(res => {
-            console.log(res.data);
-            this.setState({
-                reviews: res.data.slice(0,20),
-                allReviews: res.data,
-                totalPages: Math.floor(res.data.length / 20)
-            })
-            this.setState({
-                ratings : {
-                    totalAverage: this.getAverage(res.data, 'overallRating'),
-                    foodAverage: this.getAverage(res.data, 'foodRating'),
-                    serviceAverage: this.getAverage(res.data, 'serviceRating'),
-                    ambianceAverage: this.getAverage(res.data, 'ambianceRating'),
-                    valueAverage: this.getAverage(res.data, 'valueRating'),
-                    noise: this.getAverage(res.data, 'noise'),
-                    recommended: Math.round((this.getAverage(res.data, 'is_recommended')) * 100)
-                }
-            }, () => this.setDynamicStarRating())
-        })
-        .catch(err => console.log(err));
-    }
-    setDynamicStarRating() {
-        let totalAverageCopy = this.state.ratings.totalAverage
-        let starsToGo = false;
-        for (let i = 0; i < 5; i++) {
-            if (totalAverageCopy - 1 < 0 && !starsToGo) {
-                totalAverageCopy > .5 ? this.state.stars.push('https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/highStar.png') : this.state.stars.push('https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/lowStar.png');
-                totalAverageCopy--;
-                starsToGo = true
-                continue;
-            } else {
-                totalAverageCopy > 0 ? this.state.stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redStar.png") : this.state.stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/greyStar.png");
-                totalAverageCopy--;
-            }
-        }
-        this.setState({stars: this.state.stars})
-    }
-    pullRestaurantInfoById() {
-        axios.get(`/restaurantInfo/${3}`)
-        .then(res => {
-            this.setState({restaurantInfo: res.data});
-            console.log(res.data, 'restaurantInfo');
-        })
-        .catch(err => console.error(err));
-    }
-    pullKeywordsById() {
-        axios.get(`/filterKeywords/${3}`)
-        .then(res => {
-            this.setState({keyWords: res.data})
-            console.log(res.data);
-        })
-        .catch(err => console.log(err));   
-    }
-    pullMenuItemsById() {
-        axios.get(`/LovedFor/${3}`)
-        .then(res => {
-            this.setState({lovedFor: res.data})
-            console.log(res.data, 'lovedfordata');
-        })
-        .catch(err => console.log(err));    
-    }
-    filterReviewsByKeyword(target) {
-        if (!this.state.is_filtered) {
-            let filtered = this.state.reviews.filter((review) => review.reviewText.includes(target))
-            this.setState({reviews: filtered, is_filtered: !this.state.is_filtered});
-        } else {
-            this.setState({reviews: this.state.allReviews});
-            this.setState({is_filtered: !this.state.is_filtered})
-        }
-        console.log('filter reviews called', target);
-    }
-    filterReviewsByRating(target) {
-        let filtered = this.state.allReviews.filter((review) => review.overallRating === target);
-        this.setState({reviews: filtered});
-    }
-    sortReviewsBySelect(sortMethod) {
-        console.log('sortreviews called', sortMethod)
-        // const sortMethod = document.getElementById('dropdownHeader').textContent;
-        if (sortMethod === 'Highest') {
-            this.state.reviews.sort((a, b) => b.overallRating - a.overallRating)
-        } else if (sortMethod === 'Lowest') {
-            this.state.reviews.sort((a, b) => a.overallRating - b.overallRating)
-        } else {
-            this.state.reviews.sort((a, b) => b.dinedDate - a.dinedDate)
-        }
-        this.setState({reviews: this.state.reviews})
-    }
-    handlePageChange(page) {
+    this.setState({ stars: this.state.stars });
+  }
+
+  pullDataById() {
+    axios.get(`/reviews/${3}`)
+      .then((res) => {
+        console.log(res.data);
         this.setState({
-            reviews: this.state.allReviews.slice((page - 1) * 20, page * 20),
-            currentPage: page
-        })
-    }
-    scrollToTopOfFeed() {
-        document.getElementById('reviewContainer').scrollIntoView({behavior: 'smooth'});
-    }
-    render() {
-        return (
-            <div id="appMasterContainer">
-                <ErrorBoundary>
-                    <ReviewSummary 
-                        reviews={this.state.reviews}
-                        allReviews={this.state.allReviews} 
-                        ratings={this.state.ratings} 
-                        stars={this.state.stars}
-                        lovedFor={this.state.lovedFor}
-                        filter={this.filterReviewsByRating.bind(this)}
-                        scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}
-                        restaurantInfo={this.state.restaurantInfo}/>
-                </ErrorBoundary>
+          reviews: res.data.slice(0, 20),
+          allReviews: res.data,
+          totalPages: Math.floor(res.data.length / 20),
+        });
+        this.setState({
+          ratings: {
+            totalAverage: this.getAverage(res.data, 'overallRating'),
+            foodAverage: this.getAverage(res.data, 'foodRating'),
+            serviceAverage: this.getAverage(res.data, 'serviceRating'),
+            ambianceAverage: this.getAverage(res.data, 'ambianceRating'),
+            valueAverage: this.getAverage(res.data, 'valueRating'),
+            noise: this.getAverage(res.data, 'noise'),
+            recommended: Math.round((this.getAverage(res.data, 'is_recommended')) * 100)
+          },
+        }, () => this.setDynamicStarRating());
+      })
+      .catch(err => console.log(err));
+  }
 
-                <ErrorBoundary>
-                    <ReviewToolbar 
-                        keyWords={this.state.keyWords} 
-                        sortReviews={this.sortReviewsBySelect.bind(this)}
-                        filterReviews={this.filterReviewsByKeyword.bind(this)}
-                        scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}/>
-                </ErrorBoundary>
+  pullRestaurantInfoById() {
+    axios.get(`/restaurantInfo/${3}`)
+      .then((res) => {
+        this.setState({ restaurantInfo: res.data });
+        console.log(res.data, 'restaurantInfo');
+      })
+      .catch(err => console.error(err));
+  }
 
-                <ErrorBoundary>
-                    <ReviewList 
-                        reviews={this.state.reviews}/>  
-                </ErrorBoundary>
+  pullKeywordsById() {
+    axios.get(`/filterKeywords/${3}`)
+      .then((res) => {
+        this.setState({ keyWords: res.data });
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  }
 
-                <ErrorBoundary> 
-                    <Pagination 
-                        reviews={this.state.allReviews}
-                        handlePageChange={this.handlePageChange.bind(this)}
-                        currentPage={this.state.currentPage}
-                        totalPages={this.state.totalPages}
-                        scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}/>
-                </ErrorBoundary>
-            </div>
-        )
+  pullMenuItemsById() {
+    axios.get(`/LovedFor/${3}`)
+      .then((res) => {
+        this.setState({ lovedFor: res.data });
+        console.log(res.data, 'lovedfordata');
+      })
+      .catch(err => console.log(err));
+  }
+
+  filterReviewsByKeyword(target) {
+    if (!this.state.is_filtered) {
+      let filtered = this.state.reviews.filter(review => review.reviewText.includes(target));
+      this.setState({ reviews: filtered });
+    } else {
+      this.setState({ reviews: this.state.allReviews });
     }
+    this.setState({ is_filtered: !this.state.is_filtered });
+    console.log('filter reviews called', target);
+  }
+
+  filterReviewsByRating(target) {
+    const filtered = this.state.allReviews.filter(review => review.overallRating === target);
+    this.setState({ reviews: filtered });
+  }
+
+  sortReviewsBySelect(sortMethod) {
+    if (sortMethod === 'Highest') {
+      this.state.reviews.sort((a, b) => b.overallRating - a.overallRating);
+    } else if (sortMethod === 'Lowest') {
+      this.state.reviews.sort((a, b) => a.overallRating - b.overallRating);
+    } else {
+      this.state.reviews.sort((a, b) => b.dinedDate - a.dinedDate);
+    }
+    this.setState({ reviews: this.state.reviews });
+  }
+
+  handlePageChange(page) {
+    this.setState({
+      reviews: this.state.allReviews.slice((page - 1) * 20, page * 20),
+      currentPage: page
+    });
+  }
+
+  scrollToTopOfFeed() {
+    document.getElementById('reviewContainer').scrollIntoView({ behavior: 'smooth' });
+  }
+
+  render() {
+    return (
+      <div id="appMasterContainer">
+        <ErrorBoundary>
+          <ReviewSummary
+            reviews={this.state.reviews}
+            allReviews={this.state.allReviews}
+            ratings={this.state.ratings}
+            stars={this.state.stars}
+            lovedFor={this.state.lovedFor}
+            filter={this.filterReviewsByRating.bind(this)}
+            scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}
+            restaurantInfo={this.state.restaurantInfo}/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <ReviewToolbar
+            keyWords={this.state.keyWords}
+            sortReviews={this.sortReviewsBySelect.bind(this)}
+            filterReviews={this.filterReviewsByKeyword.bind(this)}
+            scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <ReviewList
+            reviews={this.state.reviews}/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Pagination
+            reviews={this.state.allReviews}
+            handlePageChange={this.handlePageChange.bind(this)}
+            currentPage={this.state.currentPage}
+            totalPages={this.state.totalPages}
+            scrollToTopOfFeed={this.scrollToTopOfFeed.bind(this)}/>
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 export default App;
