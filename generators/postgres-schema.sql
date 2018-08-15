fd7955fadc78497a21902038308fc52de6a78b09
Cheyenne Noel Chun <cheyennenoel@gmail.com>
2018-08-14 22:03:49 -0700

    Remove unnecessary spacing

--- generators/postgres-schema.sql
+++ generators/postgres-schema.sql
@@ -11,11 +11,8 @@ CREATE TABLE LovedFor (id SERIAL,menuItem VARCHAR(30) NOT NULL,rest_id int NOT N
 CREATE TABLE Reviews (id SERIAL,userName VARCHAR(50) NOT NULL,userPhoto VARCHAR(400) NOT NULL,userArea VARCHAR(50) NOT NULL,reviewText VARCHAR(2000) NOT NULL,is_recommended int NOT NULL default 0,dinedDate DATE NOT NULL,is_helpful int NOT NULL default 0,overallRating int NOT NUll,foodRating int NOT NUll,serviceRating int NOT NUll,ambianceRating int NOT NUll,valueRating int NOT NUll,noise int NOT NUll,userReviewCount int NOT NULL,rest_id int NOT NULL);
 
 \copy Restaurant(id,restaurantName,restaurantArea) FROM '/data/listings.csv' DELIMITER ',' CSV HEADER;
-
 \copy Filters(id,filterKeyword,rest_id) FROM '/data/all-filters.csv' DELIMITER ',' CSV HEADER;
-
 \copy LovedFor (id,menuItem,rest_id) FROM '/data/all-items.csv' DELIMITER ',' CSV HEADER;
-
 \copy Reviews (id,userName,userPhoto,userArea,reviewText,is_recommended,dinedDate,overallRating,foodRating,serviceRating,ambianceRating,valueRating,noise,userReviewCount,rest_id) FROM '/data/a-reviews.csv' DELIMITER ',' CSV HEADER QUOTE '"';
 \copy Reviews (id,userName,userPhoto,userArea,reviewText,is_recommended,dinedDate,overallRating,foodRating,serviceRating,ambianceRating,valueRating,noise,userReviewCount,rest_id) FROM '/data/b-reviews.csv' DELIMITER ',' CSV HEADER QUOTE '"';
 \copy Reviews (id,userName,userPhoto,userArea,reviewText,is_recommended,dinedDate,overallRating,foodRating,serviceRating,ambianceRating,valueRating,noise,userReviewCount,rest_id) FROM '/data/c-reviews.csv' DELIMITER ',' CSV HEADER QUOTE '"';
